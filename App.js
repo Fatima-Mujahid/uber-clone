a33021ffdb0e6fc9099d88a454b03a611d94a443
Fatima-Mujahid <65939673+Fatima-Mujahid@users.noreply.github.com>
2021-09-13 20:02:23 +0500

    feat(HomeScreen) create home screen component

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './slices/screens/HomeScreen';
 import { store } from './store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's Build REDUX!</Text>
-      </View>
+      <HomeScreen />
     </Provider>
   );
 }
---
+++ slices/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: 'contain',
+          }}
+          source={{ uri: 'https://links.papareact.com/gzs' }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
